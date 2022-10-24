a98232644df877a1dd25f731a2cfc3533022ab88
Milky Raven <heartonllamas@gmail.com>
2022-10-24 15:42:58 +0200

    last step completed

--- javascript/index.js
+++ javascript/index.js
@@ -62,12 +62,37 @@ obtainInstruction('steak', 0)
 .then ( (step7) => {
   document.querySelector("#steak").innerHTML += `<li>${step7}</li>`
   document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`;
+  document.querySelector("#steakImg").removeAttribute("hidden");
 })
+
+.catch((err) => console.log(err));
 
 
 
 // Iteration 3 using async/await
 // ...
+async function makeBroccoli(func) {
+  try {
+  const step1 = await obtainInstruction('broccoli', 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`
+  const step2 = await obtainInstruction('broccoli', 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`
+  const step3 = await obtainInstruction('broccoli', 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`
+  const step4= await obtainInstruction('broccoli', 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`
+ const step5=  await obtainInstruction('broccoli', 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`
+  const step6=  await obtainInstruction('broccoli', 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+  } catch(err){
+    console.log(err);
+  }
+}
+makeBroccoli();
+
 
 // Bonus 2 - Promise all
 // ...
